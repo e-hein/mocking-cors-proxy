11d2ad6bf6947c82aa883b35ec1fa1170ba616b9
Emanuel Hein <github@e-hein.de>
2019-06-03 09:45:59 +0200

    increase timeout

--- lib/cli.spec.ts
+++ lib/cli.spec.ts
@@ -58,7 +58,7 @@ function startProxy(cmd = "npm run cli", url = defaults.testUrl, port = defaults
 }
 
 describe("proxy cli start", function() {
-  this.timeout(5000);
+  this.timeout(10000);
 
   it("should show help", function(done) {
     shelljs.exec("npm run cli -- --help", { silent }, (exitCode, stdout, stderr) => {
