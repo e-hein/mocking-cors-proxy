6f7f4b37200e47ba5d1a164bc1e9abc62131fe4b
Emanuel Hein <github@e-hein.de>
2020-05-08 18:38:35 +0200

    log output on startup

--- lib/cli-runner.ts
+++ lib/cli-runner.ts
@@ -1,5 +1,6 @@
 import commander from "commander";
 import * as fs from "fs";
+import * as path from "path";
 import { MockingCorsProxy } from "./proxy";
 import { MockingCorsProxyConfig } from "./proxy-config.model";
 import { StaticRoute } from "./static-route.model";
@@ -12,24 +13,36 @@ function endpointMapping(val: string) {
   return mapping;
 }
 
+function getConfig() {
+  const config = new MockingCorsProxyConfig();
+
+  const configFile = commander.config;
+  if (configFile) {
+    console.log("use config file:", path.resolve(configFile));
+    const userConfig = JSON.parse(fs.readFileSync(commander.config, "utf-8"));
+    Object.assign(config, userConfig);
+  }
+
+  return config;
+}
+
 export function run(args: string[]) {
   commander
-    .version("0.0.1")
+    .version("0.0.2")
     .option("-m, --map <...path to target>", "statically map an endpoint to real targets", endpointMapping)
     .option("-p, --port <port>", "start on alternative port (default is 2345")
     .option("-c, --config <configJson>", "use config file")
   ;
 
   commander.parse(args);
-  const config = commander.config
-    ? Object.assign(new MockingCorsProxyConfig(), JSON.parse(fs.readFileSync(commander.config, "utf-8")))
-    : new MockingCorsProxyConfig()
-  ;
+  const config = getConfig();
   const proxy = new MockingCorsProxy(config);
   if (commander.map) {
     mappings.forEach((mapping: StaticRoute) => proxy.registerStaticRoute(mapping.from, mapping.to));
   }
-  proxy.listen(commander.port || config.port);
+  const port = commander.port || config.port || "2345";
+  proxy.listen(port);
+  console.log("mocking-cors-proxy runnin on " + port);
   process.on("SIGTERM", function() {
     proxy.close();
   });
